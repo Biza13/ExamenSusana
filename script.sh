745c934f68ee93b6b9f78109a600350647841d9e
Bego <111123343+Biza13@users.noreply.github.com>
2024-12-11 20:20:10 +0100

    Update script.sh
    a

--- script.sh
+++ script.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 sudo yum update -y
-sudo amazon-linux-extras install apache2 -y
+sudo yum install httpd -y
 sudo amazon-linux-extras enable php8.0
 sudo yum install php php-fpm -y
 sudo systemctl start php-fpm
 sudo systemctl enable php-fpm
-sudo systemctl restart apache2
-sudo systemctl enable apache2
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
 
 sudo yum install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker run --rm -v "$(pwd):/data" "phpdoc/phpdoc:3"
+
+docker run --rm -v "$(pwd):/data" phpdoc/phpdoc:3 -d ./var/www/html/modelo -t ./var/www/html/docs
