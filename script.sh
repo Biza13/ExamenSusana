6f8bccd35738cc68432d1953bab54887906a150c
Biza_13 <serrano_0_1@hotmail.com>
2024-12-11 12:45:29 +0100

    modificacion nombre bucket

--- script.sh
+++ script.sh
@@ -7,3 +7,9 @@ sudo systemctl start php-fpm
 sudo systemctl enable php-fpm
 sudo systemctl restart apache2
 sudo systemctl enable apache2
+
+sudo yum install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
